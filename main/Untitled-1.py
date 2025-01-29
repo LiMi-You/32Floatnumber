bb2ae825aa8e3be4f6b6c36e043329c5bc2620a9
LiMi-You <3491177636@qq.com>
2025-01-29 12:55:51 +0800

    Update Untitled-1.py

--- main/Untitled-1.py
+++ main/Untitled-1.py
@@ -8,15 +8,18 @@ Integer_list = []
 
 while(i == 0):
     u += 1
+    # 计算乘2
     jishaun = float(xiaoshu) * 2
     
-
+    # 转换类型
     value_str = str(jishaun)
-
+    # 输出当前分割值
     print( "第" + str(u) + "次计算结果值:" + str(jishaun))
 
+    # 检查是否通过文本检查
     if '.' in value_str:
 
+        # 分割对应字符串，分别对应至前述参数
         integer_part, fractional_part = value_str.split('.')
         
         ty = value_str.split('.')
